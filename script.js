146b4ca1f86da5fe0b620f87bf6d79475470f139
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-14 20:31:58 -0300

    exemplos de escopos

--- script.js
+++ script.js
@@ -2,7 +2,31 @@
 
 function calcAge(brithYear) {
   const age = 2037 - brithYear;
-  console.log(firstName);
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${brithYear}`;
+    console.log(output);
+
+    if (brithYear >= 1981 && brithYear <= 1995) {
+      var millenial = true;
+
+      output = 'New OUTPUT';
+
+      const firstName = 'steven';
+      const str = `oh, and you're a millennial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+
+  printAge();
+
   return age;
 }
 
