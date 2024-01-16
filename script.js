6cac172fae46d1bbebf8b823780ecaf8a6021f01
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-16 16:32:00 -0300

    The this Keyword in Practice

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function calcAge(brithYear) {
 const firstName = 'jonas';
 calcAge(1995);
 
-console.log(me);*/
+console.log(me);
 
 //////////////////////////////////////
 // Hoisting and TDZ in Practice
@@ -80,3 +80,43 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+
+
+// The this Keyword in Practice
+
+// console.log(this);
+
+const calcAge = function (brithYear) {
+  console.log(2037 - brithYear);
+  // console.log(this);
+};
+calcAge(1995);
+
+console.log(this);
+
+const calcAgeArrow = brithYear => {
+  console.log(2037 - brithYear);
+  // console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();*/
+
+// Regular Functions vs. Arrow Functions
